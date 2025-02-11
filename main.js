e5f7480eba15a54bcf03c1db2dbb2fe46bf6079f
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-11 14:13:02 +0100

    add main logic

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+let output;
+for (let i = 0; i <= 100; i++) {
+
+    if (i % 3 == 0) {
+        output = "Fizz";
+
+    } else if (i % 5 == 0) {
+
+        output = "Buzz";
+    } else {
+        output = i;
+    }
+
+    console.log(`the number is ${output}`);
+
+
+}
